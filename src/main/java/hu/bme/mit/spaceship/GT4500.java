31f55244a203a1b1229906f6e1ad57cb22a1261b
Szabó Gergő István <176428@BME.HU>
2023-09-27 17:27:53 +0200

    Removed TODO line comment

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        //removed TODO from this line
           if (!primaryTorpedoStore.isEmpty() && primaryTorpedoStore.fire(primaryTorpedoStore.getTorpedoCount())) {
             firingSuccess = true;
           }
